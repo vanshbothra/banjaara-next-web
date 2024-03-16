b3acb4f307bfc5de2132115d5665a60ea3995681
manyagarggg <mgmanyagarg1710@gmail.com>
2024-03-16 19:23:14 +0530

    change grid layout

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -30,22 +30,22 @@ interface festSecs{
 
   const imageStyle = {
   width: '100%',
-  height: "218px",
+  height: "268px",
   margin: 'auto',
   borderRadius: '24px',
   }
   
   const FestSecs: React.FC<festSecs> = ({ name, designation, imgSrc }) => {
     return (
-    <div className="w-full h-[220px] border border-white-600 bg-black rounded-3xl grid grid-cols-2">
+    <div className="w-full h-[270px] border border-white-600 bg-black rounded-3xl grid grid-cols-2">
         <div className="mx-auto object-contain">
         <img src={imgSrc} alt="Person" style={imageStyle} className="object-cover"></img>
         </div>
         <div className="ml-2 md:ml-3 responsive-size">
-        <div className="text-[#F72585] text-[18px] md:text-2xl">
+        <div className="text-[#F72585] text-[18px] md:text-[33px]">
             {name}
         </div>
-        <div className="text-[#FFFFFF] text-[12px] md:text-[13px]">
+        <div className="text-[#FFFFFF] text-[12px] md:text-[17px]">
             {designation}
         </div>
         </div>
@@ -53,13 +53,31 @@ interface festSecs{
     )
   }
 
+  const FestSecs2: React.FC<festSecs> = ({ name, designation, imgSrc }) => {
+    return (
+    <div className="w-full h-[270px] border border-white-600 bg-black rounded-3xl grid grid-cols-2">
+        <div className="md:ml-[9px] responsive-size">
+        <div className="text-[#F72585] text-[18px] md:text-[33px]">
+            {name}
+        </div>
+        <div className="text-[#FFFFFF] text-[12px] md:text-[17px]">
+            {designation}
+        </div>
+        </div>
+        <div className="mx-auto object-contain">
+        <img src={imgSrc} alt="Person" style={imageStyle} className="object-cover"></img>
+        </div>
+    </div>
+    )
+  }
+
   const DFS: React.FC<dfs> = ({ name, designation, imgSrc, dept }) => {
     return (
-    <div className="w-full h-[400px] md:h-[320px] border border-white-600 bg-black rounded-3xl grid grid-rows-2">
-        <div className="mx-auto w-full">
+    <div className="w-full h-[400px] md:h-[320px] border border-white-600 bg-black rounded-3xl grid grid-rows-3">
+        <div className="mx-auto w-full row-span-2">
         <img src={imgSrc} alt="Person" className="object-cover h-full w-full rounded-[24px]"></img>
         </div>
-        <div className="text-center mt-4">
+        <div className="text-center mt-4 row-start-3">
         <div className="text-[#F72585] text-[18px] md:text-2xl">
             {name}
         </div>
@@ -90,7 +108,7 @@ export default function team(){
                 Amishi Gupta 
                 </div>
           } designation="Fest Secretary"/>
-          <FestSecs imgSrc = "/team/Lekha_FS.JPG" name= {
+          <FestSecs2 imgSrc = "/team/Lekha_FS.JPG" name= {
             <><div className="responsive-div-small">
                         Lekha Siva kumar
                     </div><div className="responsive-div">
@@ -98,7 +116,7 @@ export default function team(){
                         </div></>
           } designation="Fest Secretary"/>
           </div>
-          <div className="mt-10 responsive-grid-small gap-4 relative px-4 md:px-28">
+          <div className="mt-10 responsive-grid-small relative px-4 md:px-28">
             <DFS imgSrc ="/team/Anirudh_DFS.jpg" name="Anirudh" designation="Deputy Fest Secretary" dept = "Finance, Sponsorships, and Headliners"/>
             <DFS imgSrc ="/team/Anwita_DFS.jpg" name="Anwita Gopalka" designation="Deputy Fest Secretary" dept ="PR & Outreach, Social Media and Marketing, Content and Design"/>
             <DFS imgSrc ="/team/Hubaba_DFS.jpg" name="Hubaba Masood" designation="Deputy Fest Secretary" dept = "Logistics, Decor, Legal & Documentation"/>
