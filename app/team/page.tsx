f4b0252bacbb61315a0c2617bfc195a65337ee20
vanshbothra <67708601+vanshbothra@users.noreply.github.com>
2024-03-17 19:57:02 +0530

    fix team, add images

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { Poppins } from "next/font/google";
+import { Montserrat, Poppins } from "next/font/google";
 import { Bebas_Neue } from "next/font/google";
 import { Tabs, Tab, Card, CardBody, CardHeader, Textarea, Divider, Button, Link, Chip } from "@nextui-org/react";
 import Image, { StaticImageData } from "next/image";
@@ -10,10 +10,15 @@ const poppins = Poppins({
     weight: '300'
   })
   
-  const bebas_neue = Bebas_Neue({
-    subsets: ["latin"],
-    weight: '400'
+const bebas_neue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: '400'
   })
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: "800",
+})
 
 interface festSecs{
     name: string;
@@ -39,13 +44,13 @@ interface festSecs{
     return (
     <div className="w-full h-[270px] border border-white-600 bg-black rounded-3xl grid grid-cols-2">
         <div className="mx-auto w-full">
-        <img src={imgSrc} alt="Person" style={imageStyle} className="object-cover"></img>
+          <img src={imgSrc} alt="Person" style={imageStyle} className="object-cover"></img>
         </div>
         <div className="ml-2 md:ml-3 responsive-size">
-        <div className="text-[#F72585] text-[18px] md:text-[33px]">
-            {name}
+        <div className="text-[#F72585] font-bold text-[18px] max-md:text-center md:text-[33px]">
+            Amishi <br className="max-md:hidden" /> Gupta
         </div>
-        <div className="text-[#FFFFFF] text-[12px] md:text-[17px]">
+        <div className="text-[#FFFFFF] max-md:text-center text-[12px] md:text-[17px]">
             {designation}
         </div>
         </div>
@@ -55,12 +60,12 @@ interface festSecs{
 
   const FestSecs2: React.FC<festSecs> = ({ name, designation, imgSrc }) => {
     return (
-    <div className="w-full h-[270px] border border-white-600 bg-black rounded-3xl grid grid-cols-2">
+    <div className="mb-[5vh] w-full h-[270px] border border-white-600 bg-black rounded-3xl grid grid-cols-2">
         <div className="ml-2 md:ml-3 responsive-size">
-        <div className="text-[#F72585] text-[18px] md:text-[33px]">
-            {name}
+        <div className="text-[#F72585] font-bold text-[18px] max-md:text-center md:text-[33px]">
+            Lekha <br className="max-md:hidden" /> Sivakumar
         </div>
-        <div className="text-[#FFFFFF] text-[12px] md:text-[17px]">
+        <div className="text-[#FFFFFF] max-md:text-center text-[12px] md:text-[17px]">
             {designation}
         </div>
         </div>
@@ -96,7 +101,7 @@ export default function team(){
     return (
         <main>
           <div className="flex justify-center">
-                <p className={`${bebas_neue.className} relative mt-4 text-white font-medium md:text-6xl text-6xl text-[#FFFFFF]`}>
+                <p className={`${montserrat.className} text-center relative mt-[7vh] mb-[5vh] font-medium md:text-6xl text-6xl text-[#FFFFFF]`}>
                   MEET THE TEAM
                   </p>
           </div>
@@ -104,7 +109,7 @@ export default function team(){
           <FestSecs imgSrc = "/team/Amishi_FS.jpg" name= "Amishi Gupta" designation="Fest Secretary"/>
           <FestSecs2 imgSrc = "/team/Lekha_FS.JPG" name="Lekha Sivakumar" designation="Fest Secretary"/>
           </div>
-          <div className="mt-4 md:mt-6 mb-[18px] md:mb-[28px] responsive-grid-small relative px-4">
+          <div className="mt-4 md:mt-6 mb-[4vh] md:mb-[28px] responsive-grid-small relative px-4">
             <DFS imgSrc ="/team/Anirudh_DFS.jpg" name="Anirudh" designation="Deputy Fest Secretary" dept = "Finance, Sponsorships, and Headliners"/>
             <DFS imgSrc ="/team/Anwita_DFS.jpg" name="Anwita Gopalka" designation="Deputy Fest Secretary" dept ="PR & Outreach, Social Media and Marketing, Content and Design"/>
             <DFS imgSrc ="/team/Hubaba_DFS.jpg" name="Hubaba Masood" designation="Deputy Fest Secretary" dept = "Logistics, Decor, Legal & Documentation"/>
