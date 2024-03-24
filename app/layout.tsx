25f34638f6e939685e656c9da1cba7113d89efba
vanshbothra <67708601+vanshbothra@users.noreply.github.com>
2024-03-24 22:15:18 +0530

    fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,7 @@ export default function RootLayout({
           )} */}
           <NavbarComponent/>
           {children}
+          <Analytics />
           <footer>
               <Footer />
           </footer>
